6a80d03a8f5968cc0b52518424893054dfe1a8ba
moshood23 <olaniyanmoshood2017@gmail.com>
2025-11-28 15:13:37 +0000

    initial commit

---
+++ algo/Class18.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace algo
+{
+    internal class Class18
+    {
+        public static string ListOfCharacter(List<char> list)
+        {
+            //return new string(list.ToString());
+            return string.Join(", ", list.ToArray());
+        }
+
+
+
+    }
+}
