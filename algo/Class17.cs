5b7bed7ce98637b5e6467c098eebb551936cc4f5
moshood23 <olaniyanmoshood2017@gmail.com>
2026-09-11 15:36:46 +0100

    Algo

--- algo/Class17.cs
+++ algo/Class17.cs
@@ -12,11 +12,11 @@ namespace algo
         {
             var result = list1.Except(list2).ToList();
             result.AddRange(list2.Except(list1));
-            return result;  
+            return result;
         }
 
 
-        public char[] GetUniqueElements(char[]arr1, char[] arr2)
+        public char[] GetUniqueElements(char[] arr1, char[] arr2)
         {
             var result = arr1.Except(arr2).ToList();
             result.AddRange(arr1.Except(arr2));
@@ -30,10 +30,34 @@ namespace algo
             result.AddRange(list2.Except(list1));
             return result;
         }
-    
+
+    }
+    public class Program17
+    {
+        public static void Main(string[] args)
+        {
+            var class17 = new Class17();
+            List<int> list1 = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> list2 = new List<int> { 4, 5, 6, 7, 8 };
+            var uniqueInts = class17.GetUniqueElements(list1, list2);
+            Console.WriteLine(uniqueInts.ToString());
+        }
     }
 
+    public class Program17_2
+    {
+        public static void Main(string[] args)
+        {
+            var class17 = new Class17();
+            char[] arr1 = new char[] { 'a', 'b', 'c', 'd' };
+            char[] arr2 = new char[] { 'c', 'd', 'e', 'f' };
+            var uniqueChars = class17.GetUniqueElements(arr1, arr2);
+            Console.WriteLine(uniqueChars.ToString());
+        }
+    }
 }
+        
+
 
  
 
